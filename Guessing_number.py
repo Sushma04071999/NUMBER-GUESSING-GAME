88b6c25812d8b0f960077c7ba6c9c502d0fbd315
sushma <karnatinagasushma@gmail.com>
2022-09-11 15:55:08 +0200

    Updated the changes

--- Guessing_number.py
+++ Guessing_number.py
@@ -1,10 +1,20 @@
 import random
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 #enter the name bu using the input function
-name=input('enter your name') 
-print(name,"please guess any number between 1 to 100","i will find that number",sep='\n')
+name=input('Enter your name: ') 
+print("Hello ",name,", lets predict the number you are thinking of using binary search... \n\nPlease guess any number between 1 to 100.")
 
-result="no"
+result="n"
 
 min=1
 max=100
@@ -13,31 +23,31 @@ max=100
 randomnumber=random.randint(min,max)
 
 #intial guess count 
-Guesscount=0
-
-print(randomnumber)
+Guesscount=1
 
 #using the while loop
-while result=="no":
+while result!="y":
     
     #checking the random number correct or not 
-    print("{}- is correct? type yes or no".format(randomnumber))
+    print(f"{bcolors.OKBLUE}\n{randomnumber}{bcolors.ENDC}")
+    print("Is it correct? (y/n)")
     #give input yes or no
     result=input()
     
     #if your result is equal to yes if condition executes and program execution starts 
-    if result=='yes':
-        print("Holla, i guessed number")
+    if result=='y':
+        #after how many iterations we arre gussing thw number
+        print(f"{bcolors.OKGREEN}MAZEL TOV!!{bcolors.ENDC}")
+        print("Well... it only took us {} iterations :D".format(Guesscount))
     
     #if your result==no else condtion will excute 
-    else:
+    elif result=='n':
         Guesscount=Guesscount+1
-        print("The number is greater than or less that {}".format(randomnumber))
-        print("If greater type G,or If less type L")
+        print("Is the number greater than {}? (y/n)".format(randomnumber))
         greaterorless=input()
         
         #if your guessed number is greater than generated random number then enter G then if condition starts executes
-        if greaterorless=="G":
+        if greaterorless=="y":
             #number should be between random number and max number (randomnumber,100)
             #for ex randnumber=30,if it is greater than 30,then minimum number will be 31
             min=randomnumber+1
@@ -46,12 +56,13 @@ while result=="no":
             randomnumber=random.randint(min,max)
             
         #if your guessed number is less than generated random number then enter L then if condition starts executes
-        else:
+        elif greaterorless=="n":
             #for ex randnumber=30,if it is less than 30, then max number will be 29
             max=randomnumber-1
             print("number between ",min,"and",max)
             #generates the another random number
             randomnumber=random.randint(min,max)
-    
-    #after how many iterations we arre gussing thw number     
-    print("Guessed in {} number of times".format(Guesscount))
+        else:
+            print(f"{bcolors.WARNING}Please enter the valid input.{bcolors.ENDC}")
+    else:
+        print(f"{bcolors.WARNING}Please enter the valid input.{bcolors.ENDC}")
